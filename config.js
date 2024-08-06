f2cc7f0a84e9f8e240b2085b8535fe9547b97e8a
Yosaf11 <158606655+Yosaf11@users.noreply.github.com>
2024-08-06 08:18:31 +0500

    Update config.js

--- config.js
+++ config.js
@@ -44,7 +44,113 @@ global.scan = "https://byte-session.vercel.app/";
 
 
 global.SESSION_ID =
-  process.env.SESSION_ID ||
+  process.env.SESSION_ID ||"
+const fs = require("fs-extra");
+if (fs.existsSync(".env"))
+  require("dotenv").config({ path: __dirname + "/.env" });
+global.audio = "";
+global.video = "";
+global.port = process.env.PORT;
+global.appUrl = process.env.APP_URL || "";
+global.email = "TalkDrove@gmail.com";
+global.location = "Lahore,Pakistan.";
+global.mongodb = process.env.MONGODB_URI || "mongodb+srv://Hamza:3800380ww@cluster0.uwommwq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+global.allowJids = process.env.ALLOW_JID || "923072380380@s.whatsapp.net";
+global.blockJids = process.env.BLOCK_JID || "null";
+global.DATABASE_URL = process.env.DATABASE_URL || "";
+global.timezone = process.env.TZ || process.env.TIME_ZONE || "Asia/Karachi";
+global.github = process.env.GITHUB || "https://github.com/HyHamza/BYTE-MD";
+global.gurl = process.env.GURL || "https://chat.whatsapp.com/DOWUAybfsIK3BJlnqkSyVd";
+global.website = process.env.GURL || "https://chat.whatsapp.com/DOWUAybfsIK3BJlnqkSyVd";
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://raw.githubusercontent.com/HyHamza/HyHamza/main/Images/logo.jpg";
+global.devs = "923072380380";
+global.sudo = process.env.SUDO || "923075134110";
+global.owner = process.env.OWNER_NUMBER || "923115971525";
+global.style = process.env.STYLE || "3";
+global.gdbye = process.env.GOODBYE || "false";
+global.wlcm = process.env.WELCOME || "false";
+global.warncount = process.env.WARN_COUNT || 3;
+global.disablepm = process.env.DISABLE_PM || "false";
+global.disablegroup = process.env.DISABLE_GROUPS || "false",
+global.MsgsInLog = process.env.MSGS_IN_LOG || "false";
+global.userImages = process.env.USER_IMAGES || "https://telegra.ph/file/fe157819027c6a7ebeaef.jpg";
+global.waPresence = process.env.WAPRESENCE || "available";
+global.readcmds = process.env.READ_COMMAND || "true";
+global.readmessage = process.env.READ_MESSAGE || "false";
+global.readmessagefrom = process.env.READ_MESSAGE_FROM || "";
+global.read_status = process.env.AUTO_READ_STATUS || "true";
+global.save_status = process.env.AUTO_SAVE_STATUS || "false";
+global.save_status_from = process.env.SAVE_STATUS_FROM || "all";
+global.read_status_from = process.env.READ_STATUS_FROM || "all";
+
+global.api_smd = "https://HyHamza.vercel.app";
+global.scan = "https://byte-session.vercel.app/";
+
+
+
+
+global.SESSION_ID =
+  process.env.SESSION_ID ||"Byte;;;eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoia1Awc2RLUjMraEFjdk52QU96UEtIaVVDVWN4d2N6eXd3czF3cFVsZXVVND0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiL2RrMTZDNm41MStJSXUvTVRRMjJSYzBaSXZ4eWJzZXVIMzJ5VTRBWmpHWT0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ3SWJuN0FoajZ2MjRqOFEwNVFuWnptL29hK25kNlZCU0dUTzZyWVZZWEVzPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJLOFdBNTA5bEZoVXh6WkNBaUJydEplRE9nSHRPMVAzQ3UrdUxRZy96c1ZrPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IktEakhGNjNEN2ZYVlVzREFxSk05eHd2eS9wc0hPd3RhdmpjaXRicUxySGM9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlF0MHY1MFFVbjlXRGxjT1FHd09FbXVyb0o5aUs1ekJGVDZZcUR6WVRKVjQ9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiZUIzbC82RW53T1QrNnlUUjdaSm5BWjgxUzZudWl0dHhpQ1BtQkl3WExGUT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoic3J0elM5TDhrZEJzZWpiS0I3LzdFWnRSSU1ocEZyaDBybVp1SnlHK2N5ST0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlA4eXp5TFIrN2NWUGYxS3NzUXZWMGFwdnFmMld6REV3NDZuQzJ0MW5QMUxwajd2cTRjaW9XOFZTZHRvanJtMVNlVlZVaHBSVDl4ZGFWaTNDc2FjOWdBPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MjA3LCJhZHZTZWNyZXRLZXkiOiJxUkJCQlNjQWluZHNsN21BV0RlUnV2TWd6MEFJcXpZa3RHTW82RTNIc2xJPSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJjaGV1MWdlTVFidUc5VlNRMUVkQW5nIiwicGhvbmVJZCI6ImI5OTdlN2ZhLTdmZmYtNDM1Yi1iNzljLWU2NjQ3YzU3MWY3YSIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJUZUJTdHFaNC9ZMHhFSW03UjEvdTl2VzNEd289In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiTHdnaXplMlo2azJlM2k3WS9XTnVQRlhrU1JVPSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IkJQTFhISlpOIiwibWUiOnsiaWQiOiI5MjMxMTU5NzE1MjU6MzZAcy53aGF0c2FwcC5uZXQifSwiYWNjb3VudCI6eyJkZXRhaWxzIjoiQ09uWHBiMEJFUHlneHJVR0dBRWdBQ2dBIiwiYWNjb3VudFNpZ25hdHVyZUtleSI6Ijl3cHZsdHFNWVhSTFo5OVpiZ21tVkdEaDluaFpsMDBoVFBoMUZGcWovV1k9IiwiYWNjb3VudFNpZ25hdHVyZSI6IkNsOTA3QnBIWHJUUlpuYjc3eXMwa1dzMkhBOUVaSUtSZGRhZ0hMdUM4Qy9IYkRBbjA5YnFJV2t5bHNOMm5KVlk3VG90Z2l2V1lnaVJ3b2xSK3dnT0JBPT0iLCJkZXZpY2VTaWduYXR1cmUiOiJFb21LRk9Cc1p4VlJ5aXBOazcwSU1NbCtITG1DU2RPc2JwMm1MZkliRzNWdU9uY0lVQUU1Y3luVHl0c3dkZm5tTjRMeFg5OHJCblpkai9Bdy9LVUxndz09In0sInNpZ25hbElkZW50aXRpZXMiOlt7ImlkZW50aWZpZXIiOnsibmFtZSI6IjkyMzExNTk3MTUyNTozNkBzLndoYXRzYXBwLm5ldCIsImRldmljZUlkIjowfSwiaWRlbnRpZmllcktleSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJmY0tiNWJhakdGMFMyZmZXVzRKcGxSZzRmWjRXWmROSVV6NGRSUmFvLzFtIn19XSwicGxhdGZvcm0iOiJhbmRyb2lkIiwibGFzdEFjY291bnRTeW5jVGltZXN0YW1wIjoxNzIyOTEyOTA1fQ=="
+
+
+
+
+
+  
+
+
+//___________________________________________________________________________________________________
+
+
+  
+  "Your session ID" //Make sure session id starts with Byte;;;
+
+
+
+//________________________________________________________________________
+
+
+
+
+
+
+
+
+
+module.exports = {
+  menu: process.env.MENU || "",
+
+  //Prefix variable
+  HANDLERS: process.env.PREFIX || ".",
+  BRANCH: process.env.BRANCH || "main",
+  VERSION: process.env.VERSION || "1.0.0",
+  caption: process.env.CAPTION || "*Powered By TalkDrove*",
+  author: process.env.PACK_AUTHER || "MY OWNER Create by MR.YOUSAF KHAN BALOCH 03115971525 ",
+  packname: process.env.PACK_NAME || "♥️Mr.YOUSAF MUSA KHAN",
+  botname: process.env.BOT_NAME || "BYTE-MD",
+  ownername: process.env.OWNER_NAME || "MR.YOUSAF BALOCH",
+  errorChat: process.env.ERROR_CHAT || "",
+  KOYEB_API: process.env.KOYEB_API || "false",
+  REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
+  antilink_values: process.env.ANTILINK_VALUES || "all",
+  HEROKU: process.env.HEROKU_APP_NAME && process.env.HEROKU_API_KEY,
+  aitts_Voice_Id: process.env.AITTS_ID || "37",
+  ELEVENLAB_API_KEY: process.env.ELEVENLAB_API_KEY || "",
+  WORKTYPE: process.env.WORKTYPE || process.env.MODE || "public",
+  LANG: (process.env.THEME || "BYTE").toUpperCase(),
+};
+global.rank = "updated";
+global.isMongodb = false;
+let file = require.resolve(__filename);
+fs.watchFile(file, () => {
+  fs.unwatchFile(file);
+  console.log(`Update'${__filename}'`);
+  delete require.cache[file];
+  require(file);
+});"
 
 
 
